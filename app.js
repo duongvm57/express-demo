b0e191f90fa7957ae02bd919f5e8be9c87e226d1
Vu Duong <72616826+duongvm57@users.noreply.github.com>
2023-06-28 01:17:50 -0700

    Update main.yml

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const express = require('express')
+import express from 'express';
 
 const { PORT = '3000' } = process.env
 const app = express()
 
 app.use((req, res, next) => {
-  res.send('Hello')
+  res.send('Welcome To The New World')
 })
 
 app.listen(PORT)
