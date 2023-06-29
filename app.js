108fed2b0011066da710fc3a3f4b5cb1b8099039
duongvm <duongvm@hblab.vn>
2023-06-29 18:02:07 +0700

    mongo dotenv

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 'use strict'
 
 import express from 'express';
+import { MongoClient } from 'mongodb';
+import 'dotenv/config';
 
 const { PORT = '3000' } = process.env
+
 const app = express()
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+    console.log(movie);
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.use((req, res, next) => {
   res.send('Welcome To The New World')
